8501ca57c6243e06e1959cc5ab8553d316ea7b7d
jiangziwei <jiangziwei@meituan.com>
2018-03-10 00:09:19 +0800

    多层对象的时间旅行实现

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import Lazyman from '../action/Lazyman';
+import React from 'react'
+import ReactDom from 'react-dom'
+import Lazyman from '../action/Lazyman'
 require('../action/findMost')
 var App = React.createClass({
-    render: function() {
-        return (
-            <div>
-            </div>
-        );
-    }
-});
+  render: function() {
+    return <div />
+  }
+})
 
 //最终渲染
-ReactDom.render((
-    <App />
-), document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'))
